ad7185cf3f575d7f72e133058427029d22e4feaf
Suwisa Maliton <61160128@go.buu.ac.th>
2021-09-22 12:09:32 +0700

    Work Set 21st 2021

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello world!!!')
 })
 
+app.get('/users', (req, res) => {
+    res.send('No user in the system')
+})
+
 app.listen(port, () => {
     console.log('The app is up and running!')
 })
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -6,3 +6,9 @@ describe('GET /', () => {
         request(app).get('/').expect('Hello world!!!', done)
     })
 })
+
+describe('GET /users', () => {
+    it('responds with "No user in the system"', (done) => {
+        request(app).get('/users').expect('No user in the system', done)
+    })
+})
